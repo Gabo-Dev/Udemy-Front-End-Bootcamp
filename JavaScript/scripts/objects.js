2aaeeeb1c8bfcdd727f2bcb605ad9bd65908b43b
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-05-22 18:36:44 +0200

    Update: Added objects introduction

---
+++ JavaScript/scripts/objects.js
@@ -0,0 +1,8 @@
+var car = {type:"Acura", model:"Integra", color:"white"};
+document.getElementById("demo").innerHTML = car.type;
+/*
+    we can output multiple object property values in a single line of text as shown in this script
+*/
+var person ={firstname:"Jonathan", lastname:"Orna", age:26, eyeColor:"brown"}
+document.getElementById("person").innerHTML =
+person.firstname + " has " + person.age + " years old."
