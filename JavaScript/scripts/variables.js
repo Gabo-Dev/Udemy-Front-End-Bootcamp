3013de82a6a5f51f1e74b0526ec1d7e0051935f1
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-05-22 13:51:48 +0200

    Added Variables lesson

---
+++ JavaScript/scripts/variables.js
@@ -0,0 +1,12 @@
+var x = 10;
+var y = 15;
+var z = x + y;
+
+/* 
+Rules for identifiers:
+ * Names can contain letter, digits, underscores and dollar signs.
+ * Names must begin with a letter.
+ * Names are case sensitive (y an Y are different).
+ * Reserved words cannot be used as names.
+*/
+document.getElementById("demo").innerHTML = z;
