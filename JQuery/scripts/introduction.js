ccb0e1463652c08d25d965e925f8281777f522cf
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-05-23 13:59:39 +0200

    Update jQuery's selectors lessons

---
+++ JQuery/scripts/introduction.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $("p").hide();
+        // to hide spe elements we use the id or class as selectors
+        $("#head").hide();
+    });
+});
