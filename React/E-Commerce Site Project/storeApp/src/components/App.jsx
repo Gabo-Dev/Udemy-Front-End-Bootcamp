966ea3b5352dd0819a32751c1b6a29995fb61483
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-06-19 13:49:15 +0200

    Dealing with errors in Fetch API

--- React/E-Commerce Site Project/storeApp/src/components/App.jsx
+++ React/E-Commerce Site Project/storeApp/src/components/App.jsx
@@ -1,41 +1,41 @@
 import React, { useState } from "react";
 import '../css/styles.css'
 import Category from "./Category";
-import { Fetcher } from "../fetcher";
+import { getCategories,getProducts } from "../fetcher";
 
 export function App() {
     // to manipulate our JSON data when it come back from our JSON server
-    const [categories, setCategories] = useState([]);
-    const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState({ errorMessage: '', data: [] });
+    const [products, setProducts] = useState({ errorMessage: '', data: [] });
 
     // fetch API
     React.useEffect(() => {
         // to get properly ur data back
-        const fetchData  = async() =>{
-            const data = await Fetcher("/categories");
-            setCategories(data);
+        const fetchData = async () => {
+            const responseObject = await getCategories();
+            setCategories(responseObject);
         }
         fetchData();
     }, [])
 
     const handleCategoryClick = (id) => {
         // query to check category id of each product
-        fetch("http://localhost:5000/products?catId="+id)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setProducts(data)
-        })
+        // to get properly ur data back
+        const fetchData = async () => {
+            const responseObject = await getProducts(id);
+            setProducts(responseObject);
+        }
+        fetchData();
     }
 
-    const renderCategories = () =>{
-        return categories.map(c =>
-            <Category key={c.id} id={c.id} title={c.title} onCategoryClick={() => handleCategoryClick(c.id)}/>
+    const renderCategories = () => {
+        return categories.data.map(c =>
+            <Category key={c.id} id={c.id} title={c.title} onCategoryClick={() => handleCategoryClick(c.id)} />
         )
     }
 
-    const renderProducts = () =>{
-        return products.map(p =>
+    const renderProducts = () => {
+        return products.data.map(p =>
             <div>{p.title}</div>
         )
     }
@@ -47,15 +47,17 @@ export function App() {
             </header>
             <section>
                 <nav>
-                   {categories && renderCategories()}
+                    {categories.errorMessage && <div>Error: {categories.errorMessage}</div>}
+                    {categories.data && renderCategories()}
                 </nav>
                 <article>
+                    {products.errorMessage && <div>Error: {products.errorMessage}</div>}
                     <h1>Products</h1>
-                    { products && renderProducts()}
+                    {products && renderProducts()}
                 </article>
             </section>
             <footer>
-            <p>&copy; 2024 Gabo S.L.</p>
+                <p>&copy; 2024 Gabo S.L.</p>
             </footer>
         </>
     )
--- React/E-Commerce Site Project/storeApp/src/fetcher.jsx
+++ React/E-Commerce Site Project/storeApp/src/fetcher.jsx
@@ -1,7 +1,26 @@
 const BASE_URL = "http://localhost:5000";
-export const Fetcher = async(url) =>{
-    // async call, we update the results using the data 
-    const response = await fetch(BASE_URL + url);
-    const responseData =await response.json();
-    return responseData;
+export const Fetcher = async (url) => {
+    let responseObject = { errorMessage :'', data:[]};
+    try {
+        // async call, we update the results using the data 
+        const response = await fetch(BASE_URL + url);
+        if (!response.ok) {
+            throw new Error(`HTTP Error ${respomse.status}`);
+        }
+        const responseData = await response.json();
+        responseObject.errorMessage = '';
+        responseObject.data = responseData;
+        return responseObject;
+    } catch (error) {
+        // set json to manage the error
+        responseObject.errorMessage = error.message;
+        return responseObject;
+    }
+}
+
+export const getCategories = () =>{
+    return Fetcher('/categories');
+}
+export const getProducts = id => {
+    return Fetcher('/products?catId=' + id);
 }
